20618facfae24b9e481e30bd8a57f458c0a24f61
garden <lmnoptnt@gmail.com>
2024-08-11 17:09:40 +0900

    [홍정원] - SpringExample 예제 추가

--- src/main/java/com/garden/spring/ex/mybatis/repository/ReviewRepository.java
+++ src/main/java/com/garden/spring/ex/mybatis/repository/ReviewRepository.java
@@ -11,4 +11,15 @@ public interface ReviewRepository {
 	// 전달 받은 id 와 일치하는 행 조회
 	public Review slectReview(@Param("id") int id);
 
+	// 리뷰테이블에 한행 저장에 필요한 정보를 전달 받고 한 행을 insert
+	public int insertReview(
+			@Param("storeId") int storId
+			, @Param("menu") String menu
+			, @Param("userName") String userName
+			, @Param("point") double point
+			, @Param("review") String review) ;
+	
+	// 리뷰테이블에 한행 저장에 필요한 정보를 객체로 전달 받고 한행을 insert
+	public int insertReviewByObject(Review review);
+	
 }
